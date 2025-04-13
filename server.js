7ea870f0630eacca58416b8852c1130601943701
Nguyen Duong Truong Phuc <nguyenduongtruongphuc@gmail.com>
2025-04-12 23:51:44 -0400

    sever.js

--- server.js
+++ server.js
@@ -4,5 +4,12 @@ const app = express();
 
 const port = 3000;
 
-
 app.use(express.json());
+
+
+let todo = [
+    {id: 1, task: 'Buy groceries', completed: false},
+    {id: 2, task: 'Learn Express.js', completed: false}
+];
+
+// GET
