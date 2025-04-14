386b1577c744b78c9537caf740e00113fa9b15d3
Nguyen Duong Truong Phuc <nguyenduongtruongphuc@gmail.com>
2025-04-13 21:47:54 -0400

    update

--- server.js
+++ server.js
@@ -6,16 +6,30 @@ const port = 3000;
 
 app.use(express.json());
 
+// '/' is the root
+app.get('/', (req, res) => {
+  res.send('Hello from the root route!');
+})
 
-let todo = [
-    {id: 1, task: 'Buy groceries', completed: false},
-    {id: 2, task: 'Learn Express.js', completed: false}
-];
+
+/** 
+ * 
+ * app.get('/todo', (req, res) =>{
+ * res.json(todo);
+ * })
+*/
 
 // GET
 app.get('/todo', (req, res) => {
     res.json(todo);
 })
+
+let todo = [
+  {id: 1, task: 'Buy groceries', completed: false},
+  {id: 2, task: 'Learn Express.js', completed: false}
+];
+
+
 
 /*
 app.post('/todos', (req, res) => {
@@ -37,20 +51,7 @@ app.post('/todo', (req, res) => {
 });
 
 
-/* 
-app.put('/todos/:id', (req, res) => {
-  const { id } = req.params;
-  const { task, completed } = req.body;
-  const todoIndex = todos.findIndex(todo => todo.id === parseInt(id));
-  
-  if (todoIndex !== -1) {
-    // Update the todo with new values
-    todos[todoIndex] = { ...todos[todoIndex], task, completed };
-    res.json(todos[todoIndex]);
-  } else {
-    res.status(404).json({ error: 'Todo not found' });
-  }
-});*/
+
 
 app.put('/todo', (req, res)=>{
     const {id} = req.params;
@@ -64,4 +65,34 @@ app.put('/todo', (req, res)=>{
     else {
         res.status(404).json({error: 'Not found lol'});
     }
+});
+
+/*
+app.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  const todoIndex = todos.findIndex(todo => todo.id === parseInt(id));
+  
+  if (todoIndex !== -1) {
+    const deletedTodo = todos.splice(todoIndex, 1);
+    res.json(deletedTodo[0]);
+  } else {
+    res.status(404).json({ error: 'Todo not found' });
+  }
+});*/
+
+app.delete('/todo/:id', (req, res)=>{
+  const {id} = req.params;
+  const todoIndex = todo.findIndex(todo => todo.id === parseInt(id));
+  if(todoIndex !== -1){
+    const deleteTodo = todo.splice(todoIndex, 1);
+    res.json(deleteTodo[0]);
+  } else {
+    res.status(404).json({error: 'Not found lolll'});
+  }
+})
+
+
+
+app.listen(port, () => {
+  console.log( `Server is running on https://localhost:${port}`);
 });
